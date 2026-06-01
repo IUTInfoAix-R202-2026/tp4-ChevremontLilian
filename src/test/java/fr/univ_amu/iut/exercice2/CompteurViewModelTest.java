6751fc3566b18378a037207b2e2623026d49b097
Lilian Chevremont <lilian.chevremont@etu.univ-amu.fr>
2026-06-01 07:51:16 +0000

    exercice 2 terminé

--- src/test/java/fr/univ_amu/iut/exercice2/CompteurViewModelTest.java
+++ src/test/java/fr/univ_amu/iut/exercice2/CompteurViewModelTest.java
@@ -2,12 +2,16 @@ package fr.univ_amu.iut.exercice2;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
 /**
- * Test de l'exercice 2 : la logique du compteur se vérifie intégralement sa s interface graphique.
+ * Test de l'exercice 2 : la logique du compteur se vérifie intégralement sans interface graphique.
  * Chaque commande est appelée directement sur le ViewModel et on observe le {@code message}.
  */
 class CompteurViewModelTest {
 
+  @Disabled("Retire cette annotation pour activer le test")
   @Test
   void au_demarrage_le_message_affiche_zero() {
     CompteurViewModel vm = new CompteurViewModel(new Compteur());
@@ -17,6 +21,7 @@ class CompteurViewModelTest {
         .isEqualTo("Compteur à 0");
   }
 
+  @Disabled("Retire cette annotation pour activer le test")
   @Test
   void incrementer_augmente_le_compteur_de_un() {
     CompteurViewModel vm = new CompteurViewModel(new Compteur());
@@ -26,6 +31,7 @@ class CompteurViewModelTest {
     assertThat(vm.messageProperty().get()).isEqualTo("Compteur à 1");
   }
 
+  @Disabled("Retire cette annotation pour activer le test")
   @Test
   void plusieurs_increments_se_cumulent() {
     CompteurViewModel vm = new CompteurViewModel(new Compteur());
@@ -37,6 +43,7 @@ class CompteurViewModelTest {
     assertThat(vm.messageProperty().get()).isEqualTo("Compteur à 3");
   }
 
+  @Disabled("Retire cette annotation pour activer le test")
   @Test
   void decrementer_diminue_le_compteur() {
     CompteurViewModel vm = new CompteurViewModel(new Compteur());
@@ -48,6 +55,7 @@ class CompteurViewModelTest {
     assertThat(vm.messageProperty().get()).isEqualTo("Compteur à -1");
   }
 
+  @Disabled("Retire cette annotation pour activer le test")
   @Test
   void reinitialiser_remet_le_compteur_a_zero() {
     CompteurViewModel vm = new CompteurViewModel(new Compteur());
